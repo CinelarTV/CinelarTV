18454880a88ca8d9599a696cfa1a1c348a3b6750
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2023-08-22 00:00:30 -0300

    Update contents_controller.rb

--- app/controllers/admin/contents_controller.rb
+++ app/controllers/admin/contents_controller.rb
@@ -3,28 +3,34 @@
 module Admin
   class ContentsController < Admin::BaseController
     def find_recommended_metadata
-      if SiteSetting.tmdb_api_key.blank?
-        render json: { error: "TMDB API Key is not set" }, status: :unprocessable_entity
-        return
-      end
+      begin
+        if SiteSetting.tmdb_api_key.blank?
+          render json: { error: "TMDB API Key is not set" }, status: :unprocessable_entity
+          return
+        end
 
-      if params[:title].blank?
-        render json: { error: "Title is required" }, status: :unprocessable_entity
-        return
-      end
+        if params[:title].blank?
+          render json: { error: "Title is required" }, status: :unprocessable_entity
+          return
+        end
 
-      api_key = SiteSetting.tmdb_api_key.strip
-      Rails.logger.info("Using TMDB API Key: #{api_key}")
-      @config = Tmdb::Configuration.get
-      Tmdb::Api.key(api_key)
-      Tmdb::Api.language(SiteSetting.default_locale)
-      @search = Tmdb::Search.multi(params[:title])
-      puts @search
-      # Render as JSON
-      render json: {
-                data: @search.table,
-                config: @config.as_json,
-      }
+        api_key = SiteSetting.tmdb_api_key.strip
+        Rails.logger.info("Using TMDB API Key: #{api_key}")
+        @config = Tmdb::Configuration.get
+        Tmdb::Api.key(api_key)
+        Tmdb::Api.language(SiteSetting.default_locale)
+        @search = Tmdb::Search.multi(params[:title])
+        puts @search
+        # Render as JSON
+        render json: {
+                 data: @search.table,
+                 config: @config.as_json,
+               }
+      rescue Tmdb::Error => e
+        render json: { error: "Error occurred: #{e.message}" }, status: :unprocessable_entity
+      rescue StandardError => e
+        render json: { error: "An error occurred: #{e.message}" }, status: :unprocessable_entity
+      end
     end
 
     def create
