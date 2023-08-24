0ccdbdbf39345432691e50ae6f83f767c5fbe4b9
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2023-08-24 16:15:52 -0300

    Update 20230819014230_rolify_create_roles.rb

--- db/migrate/20230819014230_rolify_create_roles.rb
+++ db/migrate/20230819014230_rolify_create_roles.rb
@@ -2,7 +2,7 @@ class RolifyCreateRoles < ActiveRecord::Migration[7.0]
   def change
     create_table(:roles) do |t|
       t.string :name
-      t.references :resource, :polymorphic => true
+      t.references :resource, polymorphic: true, type: :uuid, index: true
 
       t.timestamps
     end
