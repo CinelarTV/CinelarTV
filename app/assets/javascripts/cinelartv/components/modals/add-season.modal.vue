05b91ca87535c507b2470386aaf7ad98984ea18e
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2023-09-05 19:01:14 -0300

    Update add-season.modal.vue

--- app/assets/javascripts/cinelartv/components/modals/add-season.modal.vue
+++ app/assets/javascripts/cinelartv/components/modals/add-season.modal.vue
@@ -5,7 +5,6 @@
                 leave="duration-200 ease-in" leave-from="opacity-100" leave-to="opacity-0">
                 <div class="fixed inset-0 bg-black bg-opacity-95 z-[101]" />
             </TransitionChild>
-
             <div class="fixed inset-0 overflow-y-auto z-[102]">
                 <div class="flex min-h-full items-center justify-center p-4 text-center">
                     <TransitionChild as="template" enter="duration-300 ease-out" enter-from="opacity-0 scale-95"
