36f04aad60ec68ad1361106250cd44f870ad21e0
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2023-09-05 19:27:47 -0300

    Update 100_message_bus.rb

--- config/initializers/100_message_bus.rb
+++ config/initializers/100_message_bus.rb
@@ -4,6 +4,11 @@ Rails.application.config do |config|
   MessageBus.configure(backend: :memory)
 
   MessageBus.user_id_lookup do |env|
-    env["warden"].user&.id || env["rack.session"]["user_id"]
+    req = Rack::Request.new(env)
+    if req.session && req.session["warden.user.user.key"] && req.session["warden.user.user.key"][0][0]
+      user = User.find(req.session["warden.user.user.key"][0][0])
+      user.id
+      Rails.logger.info "MessageBus.user_id_lookup: #{user.id}"
+    end
   end
 end
