c8245512df86ed6b0f255be68b6783f53ca58a6b
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2023-09-07 23:40:02 -0300

    DEV: Create c-icon component

---
+++ app/assets/javascripts/cinelartv/components/c-icon.vue
@@ -0,0 +1,18 @@
+<template>
+    <span>
+        <svg class="icon" :width="size" :height="size">
+            <use :xlink:href="`#${icon}`" />
+        </svg>
+    </span>
+</template>
+  
+<script setup>
+const props = defineProps({
+    icon: String,
+    size: {
+        type: [Number, String],
+        default: 18,
+    },
+});
+</script>
+  
