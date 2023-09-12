bfac676a5f5496b43fe09dcbd7434d4a2de5128e
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2023-09-11 23:14:05 -0300

    Update logs.vue

--- app/assets/javascripts/cinelartv/views/admin/webhooks/logs.vue
+++ app/assets/javascripts/cinelartv/views/admin/webhooks/logs.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex flex-col h-full">
-        <!-- Table with event_name and payload -->
+        <!-- Table with event_name and formatted payload -->
         <table>
             <thead>
                 <tr>
@@ -11,27 +11,40 @@
             <tbody>
                 <tr v-for="(log, index) in logs" :key="index">
                     <td>{{ log.event_name }}</td>
-                    <td>{{ log.payload }}</td>
+                    <!-- Use a <pre> tag to display formatted JSON -->
+                    <td>
+                        <pre>{{ formatPayload(log.payload) }}</pre>
+                    </td>
                 </tr>
             </tbody>
         </table>
     </div>
-
 </template>
   
 <script setup>
 import { ref, computed, onMounted, inject } from 'vue'
-import { ajax } from '../../../lib/axios-setup';
+import { ajax } from '../../../lib/axios-setup'
 
 onMounted(() => {
-    fetchLogs();
-});
+    fetchLogs()
+})
 
 const logs = ref(null)
 
 const fetchLogs = async () => {
-    const response = await ajax.get('/admin/webhooks/logs.json');
-    logs.value = response.data.data;
+    const response = await ajax.get('/admin/webhooks/logs.json')
+    logs.value = response.data.data
 }
 
+// Función para formatear la cadena JSON de payload
+const formatPayload = (payload) => {
+    try {
+        const parsedPayload = JSON.parse(payload)
+        // Use JSON.stringify con espacios de sangría para formatear
+        return JSON.stringify(parsedPayload, null, 2)
+    } catch (error) {
+        return 'Invalid JSON'
+    }
+}
 </script>
+  
