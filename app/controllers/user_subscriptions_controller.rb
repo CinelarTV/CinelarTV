2ca10d5b41db269998a8a292f35aae09c6507df4
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2023-09-12 13:06:54 -0300

    Update user_subscriptions_controller.rb

--- app/controllers/user_subscriptions_controller.rb
+++ app/controllers/user_subscriptions_controller.rb
@@ -2,14 +2,23 @@
 
 class UserSubscriptionsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_subscription
 
   def index
     @subscriptions = UserSubscription.where(user_id: current_user.id)
+
+    subscription_data = get_subscription_details
+
+    if subscription_data
+      @subscriptions[:api_response] = subscription_data
+    end
+
     respond_to do |format|
       format.html
       format.json {
         render json: {
                  data: @subscriptions.as_json,
+
                }
       }
     end
@@ -27,7 +36,25 @@ class UserSubscriptionsController < ApplicationController
 
   private
 
+  def set_subscription
+    @subscription = UserSubscription.find_by(user_id: current_user.id)
+  end
+
   def subscription_params
     params.require(:user_subscription).permit(:plan_id)
+  end
+
+  def get_subscription_details
+    response = HTTParty.get("https://api.lemonsqueezy.com/v1/subscriptions/#{@subscription.order_id}")
+    # https://api.lemonsqueezy.com/v1/subscriptions/id
+
+    if response.code == 200
+      data = JSON.parse(response.body)
+      data
+    end
+  rescue StandardError => e
+    # Handle other exceptions
+    Rails.logger.error("Error while getting subscription details for user #{current_user.id}: #{e.message}")
+    return nil
   end
 end
