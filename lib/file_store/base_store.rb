f040508217814eea97aeef6cda51c579c0981d8f
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2023-09-13 14:51:10 -0300

    WIP: FileStore

---
+++ lib/file_store/base_store.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+module FileStore
+  class DownloadError < StandardError; end
+
+  class BaseStore
+    UPLOAD_PATH_REGEX ||= %r{/(original/\d+X/.*)}
+    OPTIMIZED_PATH_REGEX ||= %r{/(optimized/\d+X/.*)}
+    TEMP_UPLOAD_PATH_REGEX ||= "tmp/uploads"
+
+    def upload_path
+      path = File.join(Rails.root, "public", "uploads")
+      FileUtils.mkdir_p(path) unless File.exist?(path)
+      path
+    end
+
+    def store_file(file, path)
+      not_implemented
+    end
+
+    def delete_file(path)
+      not_implemented
+    end
+
+    def download_file(path)
+      not_implemented
+    end
+
+    def not_implemented
+      raise NotImplementedError, "This method must be implemented by subclasses"
+    end
+  end
+end
---
+++ lib/file_store/local_store.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+module FileStore
+  class LocalStore < BaseStore
+    def store_file(file, path)
+      copy_file(file, path)
+      "uploads/#{path}"
+    end
+
+    def copy_file(file, path)
+      dir = Pathname.new(path).dirname
+      FileUtils.mkdir_p(dir) unless File.exist?(dir)
+      FileUtils.open(path, "wb") { |f| f.write(file.read) }
+    end
+
+    def is_relative?(url)
+      url.present? && (!url.start_with?("http://") && !url.start_with?("https://"))
+    end
+
+    def external?
+      false # Uploads from LocalStore are always internal
+    end
+
+    def delete_file(path)
+    end
+
+    def download_file(path)
+      File.open(path, "rb")
+    end
+  end
+end
