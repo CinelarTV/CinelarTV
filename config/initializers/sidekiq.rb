0ace342cc22e07d81b9ef446648702e6c81a0022
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2023-09-16 10:44:07 -0300

    Update sidekiq.rb

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -9,12 +9,3 @@ end
 Sidekiq.configure_client do |config|
   config.redis = { url: ENV["REDIS_URL"] }
 end
-
-if Rails.env.production? && ENV.keys.any? { |key| key.start_with?("RAILWAY_") }
-  Rails.config.after_initialize do
-    unless Sidekiq::ProcessSet.new.any? || !CinelarTV.maintenance_enabled
-      Rails.logger.info("CinelarTV is running on Railway, starting Sidekiq manually...")
-      system("bundle exec sidekiq")
-    end
-  end
-end
