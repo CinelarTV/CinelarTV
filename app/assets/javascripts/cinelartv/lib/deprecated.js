82b3639b59194562fa16850ef97b0143776e49c4
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2023-09-17 20:43:19 -0300

    DEV: Add deprecated to javascript

---
+++ app/assets/javascripts/cinelartv/lib/deprecated.js
@@ -0,0 +1,31 @@
+import {reportError} from './logster.js';
+
+export default function deprecated(msg, options = {}) {
+    const { deprecatedFunction, since, dropFrom } = options;
+    msg = ["Deprecation notice:", msg];
+
+    if (since) {
+        msg.push([`(deprecated since CinelarTV ${since}`]);
+    }
+
+    if (!dropFrom && since) {
+        msg.push([`)`]);
+    }
+    if (dropFrom) {
+
+        if (since) {
+            msg.push([`, removal from CinelarTV ${dropFrom})`]);
+        }
+        else {
+
+            msg.push([`removal from CinelarTV ${dropFrom}`]);
+
+        }
+    }
+
+    msg = msg.join(' ');
+
+    reportError(new Error(msg), 'warning');
+ 
+    console.warn(msg);
+}
