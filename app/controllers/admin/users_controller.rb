cc7b5eb668a3cee83ebf032b4a57e20e253d4b52
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2023-09-23 14:07:01 -0300

    FEAT: Allow admins to create Users manually

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -9,10 +9,33 @@ module Admin
         format.html
         format.json do
           render json: {
-            data: @users.as_json(only: %i[id email username created_at updated_at])
+            data: @users.as_json(only: %i[id email username created_at updated_at]),
           }
         end
       end
+    end
+
+    # Allow admins to create users
+
+    def create_user
+      raise CinelarTV::NotFound unless SiteSetting.allow_admin_to_create_users
+
+      @user = User.new(user_params)
+      if @user.save
+        render json: {
+          data: @user.as_json(only: %i[id email username created_at updated_at]),
+        }
+      else
+        render json: {
+          data: @user.errors,
+        }
+      end
+    end
+
+    private
+
+    def user_params
+      params.require(:user).permit(:email, :username, :password)
     end
   end
 end
