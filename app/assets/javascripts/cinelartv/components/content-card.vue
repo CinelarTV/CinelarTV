92a4759575e96d825f2d4e7f6deed7e53af05def
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2023-09-22 22:29:35 -0300

    UX: Add TAB focusable on recyclerview

--- app/assets/javascripts/cinelartv/components/content-card.vue
+++ app/assets/javascripts/cinelartv/components/content-card.vue
@@ -1,35 +1,52 @@
 <template>
-    <li class="content-item">
-      <article class="recyclerview-card-article">
-        <section class="recyclerview-card-article__section">
-          <div class="recyclerview-card-article__element">
-            <RouterLink :to="`/contents/${data.id}`">
-              <div class="recyclerview-card-article__image" style="aspect-ratio:16/9">
-                <img :src="data.banner" alt="Cover Image" />
+  <li class="content-item" tabindex="0" @keydown.enter="$router.push(`/contents/${data.id}`)">
+    <article class="recyclerview-card-article">
+      <section class="recyclerview-card-article__section">
+        <div class="recyclerview-card-article__element">
+          <RouterLink :to="`/contents/${data.id}`" tabindex="-1">
+            <div class="recyclerview-card-article__image" style="aspect-ratio:16/9">
+              <img :src="data.banner" alt="Cover Image" />
+            </div>
+            <div class="recyclerview-card-article__overlay"
+              :class="data.duration && data.progress ? 'with-progress' : ''">
+              <div class="recyclerview-card-article__title">
+                {{ data.title }}
               </div>
-              <div class="recyclerview-card-article__overlay" :class="data.duration && data.progress ? 'with-progress' : ''">
-                <div class="recyclerview-card-article__title">
-                  {{ data.title }}
-                </div>
-                <div class="recyclerview-card-article__progress" v-if="data.progress && data.duration">
-                  <div class="recyclerview-card-article__progress__bar">
-                    <div class="recyclerview-card-article__progress__bar__progress" :style="`width: ${(data.progress / data.duration) * 100}%`">
-                    </div>
+              <div class="recyclerview-card-article__progress" v-if="data.progress && data.duration">
+                <div class="recyclerview-card-article__progress__bar">
+                  <div class="recyclerview-card-article__progress__bar__progress"
+                    :style="`width: ${(data.progress / data.duration) * 100}%`">
                   </div>
                 </div>
               </div>
-            </RouterLink>
-          </div>
-        </section>
-        <img :src="data.banner" class="blurred-shadow" :alt="data.title" />
-      </article>
-    </li>
-  </template>
+            </div>
+          </RouterLink>
+        </div>
+      </section>
+      <img :src="data.banner" class="blurred-shadow" :alt="data.title" />
+      <div class="expanded-info" v-if="isHovering">
+        <div class="expanded-info__title">
+          {{ data.title }}
+        </div>
+        <div class="expanded-info__description">
+          {{ data.description }}
+        </div>
+      </div>
+    </article>
+  </li>
+</template>
   
-  <script>
-  export default {
-    props: {
-      data: Object
-    },
-  };
-  </script>
+<script setup>
+import { defineProps, ref } from 'vue'
+
+const props = defineProps({
+  data: {
+    type: Object,
+    required: true
+  }
+})
+
+const isHovering = ref(false)
+
+</script>
+  
