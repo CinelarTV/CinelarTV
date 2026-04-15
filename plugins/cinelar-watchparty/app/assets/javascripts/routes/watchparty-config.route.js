1a783e13596d7fc69d1cef352cfd7dbf9c969bed
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2026-04-15 20:03:34 -0300

    feat: update watchparty route path and name for proper navigation

--- plugins/cinelar-watchparty/app/assets/javascripts/routes/watchparty-config.route.js
+++ plugins/cinelar-watchparty/app/assets/javascripts/routes/watchparty-config.route.js
@@ -1,6 +1,6 @@
 export default {
-    path: '/', // Asegúrate de que cada ruta tenga una propiedad path
-    name: 'home.index',
+    path: '/watchparty', // Asegúrate de que cada ruta tenga una propiedad path
+    name: 'watchparty.index',
     component: () => import(/* webpackChunkName: "watchparty" */ '../views/introducing-watchparty.vue'),
     meta: {
         requiresAuth: true,
