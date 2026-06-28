3c383c4e17999849b7204aa019a5e977b94f09cc
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2026-06-28 16:56:23 -0300

    bump version

--- lib/version.rb
+++ lib/version.rb
@@ -7,7 +7,7 @@ module CinelarTV
         MAJOR = 0
         MINOR = 1
         TINY = 0
-        PRE = "beta2"
+        PRE = "beta4"
 
         FULL = [MAJOR, MINOR, TINY, PRE].compact.join(".")
       end
