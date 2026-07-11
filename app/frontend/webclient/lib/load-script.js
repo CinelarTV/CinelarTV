fe3fd95cc2237a9a0d916dcdae69bc3d6b661fd2
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2026-07-11 14:17:46 -0300

    apply nonce

--- app/frontend/webclient/lib/load-script.js
+++ app/frontend/webclient/lib/load-script.js
@@ -1,9 +1,13 @@
+const getNonce = () => document.querySelector('meta[name="csp-nonce"]')?.content;
+
 const loadScript = (src, inlineCode = null) => {
     return new Promise((resolve, reject) => {
         if (src) {
             const script = document.createElement('script');
             script.src = src;
             script.async = true;
+            const nonce = getNonce();
+            if (nonce) script.setAttribute('nonce', nonce);
             script.onload = () => {
                 console.log('[Script] External script loaded: ' + src);
                 resolve();
@@ -19,6 +23,8 @@ const loadScript = (src, inlineCode = null) => {
                 const script = document.createElement('script');
                 script.text = inlineCode;
                 script.type = 'text/javascript';
+                const nonce = getNonce();
+                if (nonce) script.setAttribute('nonce', nonce);
                 document.head.appendChild(script);
                 console.log('[Script] Inline script executed');
                 resolve();
