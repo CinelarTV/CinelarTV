9985c239fa6901f976fc26a2625fd5f0959a1a66
Alexander Barrios <yanquis.barrios@estudiantes.utec.edu.uy>
2026-08-05 00:28:28 -0300

    .

--- db/migrate/20260805000001_fix_image_variants_imageable_id_type.rb
+++ db/migrate/20260805000001_fix_image_variants_imageable_id_type.rb
@@ -4,7 +4,8 @@ class FixImageVariantsImageableIdType < ActiveRecord::Migration[7.2]
   def up
     execute "DELETE FROM image_variants"
 
-    change_column :image_variants, :imageable_id, :uuid, null: false
+    execute "ALTER TABLE image_variants ALTER COLUMN imageable_id DROP DEFAULT"
+    execute "ALTER TABLE image_variants ALTER COLUMN imageable_id TYPE uuid USING imageable_id::uuid"
   end
 
   def down
